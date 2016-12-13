f76626cf7a61af74565b76e54eebe0528195ed8d
Jason Peak <jpeak5@lsu.edu>
2016-12-13 10:56:38 -0600

    adds dir param

--- gig_subfolder/subfolder.sh
+++ gig_subfolder/subfolder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+LOC=$1
 SIZE=4000
-for OUTPUT in $(ls *.zip );do 
+for OUTPUT in $(ls $LOC/*.zip );do
 #	echo ${OUTPUT}
 	if [ $(stat -c '%s' "${OUTPUT}") > $SIZE ]; then
 		if [[ ${OUTPUT} != *cpd* ]]; then
