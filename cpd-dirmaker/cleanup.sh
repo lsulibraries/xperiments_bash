7d3bade795f85f6be732ea9cf47c43fd48a458d9
flummingbird <wwconlin@gmail.com>
2016-12-13 11:20:56 -0600

    obvious renaming

---
+++ cpd-dirmaker/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+$(mv empty-cpd/empty-cpd.zip .)
+$(rm -Rf  empty-cpd)
+
+$(mv empty1-cpd/empty1-cpd.zip .)
+$(rm -Rf  empty1-cpd)
+
+$(mv empty2-cpd/empty2-cpd.zip .)
+$(rm -Rf  empty2-cpd)
---
+++ cpd-dirmaker/cpd-dirmaker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for OUTPUT in  $(ls *.zip .);
+do
+	if [[ ${OUTPUT} =~ .*cpd.* ]]; then
+		$(mkdir $(echo ${OUTPUT} | sed 's/\.[^.]*$//' ))
+		$(mv ${OUTPUT} $(echo ${OUTPUT} |  sed 's/\.[^.]*$//' ))
+	fi
+
+done
