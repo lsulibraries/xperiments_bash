04498600d1a6da37e655a233c52d8bcf9214f039
flummingbird <wwconlin@gmail.com>
2016-12-16 14:19:15 -0600

    removed commented lines, delete prints

--- auto_drush_ingest/auto-ibsp-writer.sh
+++ auto_drush_ingest/auto-ibsp-writer.sh
@@ -10,15 +10,12 @@ ext=''
 for line in $(cat input);do
 	if [[ ${line} =~ .*cpd.* ]]; then
 		namespace=$(echo ${line} |  sed 's/-cpd\.[^.]*$//' )
-		#echo ${namespace}
-		#echo 'drush -u 1 icbp --target=/tmp/'"$namespace"'-cpd --namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
+		echo 'drush -u 1 icbp --target=/tmp/'"$namespace"'-cpd --namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
 	fi
 	if [[ ${line} != *cpd* ]]; then
 		namespace=$(echo ${line} |  sed 's/\.[^.]*$//' )
 		name_ext=$(echo "${line:0:${#line}-4}")
 		ext=${name_ext: -3}
-		echo ${ext}
-		echo  ${cmodels[${ext}]}
                 echo 'drush -u 1 ibsp --type=zip --scan_target=/tmp/'"$line"' --content_model='"${cmodels[$ext]}"' -namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
         fi
 
