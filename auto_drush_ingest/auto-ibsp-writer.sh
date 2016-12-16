ec406cc14d82874264b24db92fa348765fae76dd
flummingbird <wwconlin@gmail.com>
2016-12-16 14:17:06 -0600

    creates drush command for -cpd.zip and -contenttype.zip does not handle > 1G commands of --type=directory

--- auto_drush_ingest/auto-ibsp-writer.sh
+++ auto_drush_ingest/auto-ibsp-writer.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 declare -A cmodels
 cmodels=( ['pdf']='sp_pdf' ['jp2']='sp_large_image_cmodel' ['mp4']='sp_videoCModel' ['mp3']='sp-audoCmodel')
-echo "${!cmodels[@]}"
-echo "${cmodels[@]}"
+#echo "${!cmodels[@]}"
+#echo "${cmodels[@]}"
+namespace=''
+name_ext=''
+ext=''
 
 for line in $(cat input);do
-	echo $line
+	if [[ ${line} =~ .*cpd.* ]]; then
+		namespace=$(echo ${line} |  sed 's/-cpd\.[^.]*$//' )
+		#echo ${namespace}
+		#echo 'drush -u 1 icbp --target=/tmp/'"$namespace"'-cpd --namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
+	fi
+	if [[ ${line} != *cpd* ]]; then
+		namespace=$(echo ${line} |  sed 's/\.[^.]*$//' )
+		name_ext=$(echo "${line:0:${#line}-4}")
+		ext=${name_ext: -3}
+		echo ${ext}
+		echo  ${cmodels[${ext}]}
+                echo 'drush -u 1 ibsp --type=zip --scan_target=/tmp/'"$line"' --content_model='"${cmodels[$ext]}"' -namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
+        fi
+
 done;
