7e79911d6ab987f6a89e9c3ec2fe9e4e238f1a58
flummingbird <wwconlin@gmail.com>
2016-12-16 15:37:33 -0600

    did i delete from remote?

--- dirmaker_bash/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-$(mv empty-cpd/empty-cpd.zip .)
-$(rm -Rf  empty-cpd)
-
-$(mv empty1-cpd/empty1-cpd.zip .)
-$(rm -Rf  empty1-cpd)
-
-$(mv empty2-cpd/empty2-cpd.zip .)
-$(rm -Rf  empty2-cpd)
--- dirmaker_bash/populate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-$(echo '' >> empty-cpd.zip)
-$(echo '' >> empty1-cpd.zip)
-$(echo '' >> empty2-cpd.zip)
