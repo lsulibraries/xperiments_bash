474a4b0fbcd53abd3554ac308df526e410c017fc
flummingbird <wwconlin@gmail.com>
2016-12-19 09:39:09 -0600

    added better variable names, print vars and type of collection

--- auto_drush_ingest/auto-ibsp-writer.sh
+++ auto_drush_ingest/auto-ibsp-writer.sh
@@ -10,16 +10,24 @@ ext=''
 for line in $(cat input);do
 	if [[ ${line} =~ .*cpd.* ]]; then
 		namespace=$(echo ${line} |  sed 's/-cpd\.[^.]*$//' )
+		echo 'has cpd namespace:' ${namespace}
 		echo 'drush -u 1 icbp --target=/tmp/'"$namespace"'-cpd --namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
 	fi
 	if [[ ${line} != *cpd* ]]; then
-		namespace=$(echo ${line} |  sed 's/\.[^.]*$//' )
-		name_ext=$(echo "${line:0:${#line}-4}")
+		name_ext=$(echo ${line} |  sed 's/\.[^.]*$//' )
+		namespace=$(echo "${name_ext:0:${#name_ext}-4}")
+		echo 'simple collection, namespace:'
+		echo  ${namespace}
+		echo 'simple collection, name_ext'
+		echo  ${name_ext}
 		ext=${name_ext: -3}
+		echo 'simple collection, extension'
+		echo ${ext}
                 echo 'drush -u 1 ibsp --type=zip --scan_target=/tmp/'"$line"' --content_model='"${cmodels[$ext]}"' -namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
         fi
 	if [[ ${line} != *.zip  ]]; then
 		namespace=$(echo "${line:0:${#line}-4}")
+		echo 'gig dir,  namespace:' ${namespace}
                 ext=${line: -3}
                 echo 'drush -u 1 ibsp --type=directory --scan_target=/tmp/'"$line"' --content_model='"${cmodels[$ext]}"' -namespace='"$namespace"' --parent='"$namespace"':collection' >> drush-commands
 	fi
