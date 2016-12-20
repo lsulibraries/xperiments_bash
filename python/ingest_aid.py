6c551b071ccb11f0189c45683ef2f5970a44db08
flummingbird <wwconlin@gmail.com>
2016-12-20 13:49:39 -0600

    def large_zip_subdir()

--- python/ingest_aid.py
+++ python/ingest_aid.py
@@ -1,7 +1,7 @@
 #ingest_aid for moving cpd.zips into cpd folders, and > 1Gig .zips into subfolders, automatic population of 'drush-commands' with namespace, target files, and and contentmodels
 # designed to remove human error, and save time ingesting into islandora, via drush.
 
-import os.mkdir
+import os
 
 #input = os.read()
 #input list of .zips 'ownerinstitution-cdmalias-type.zip' 'uno-p15140coll23-jp2.zip' 'lsu-p1234coll1-cpd.zip' 'loyno-morethan1Gigcoll2-mp3.zip'
@@ -9,19 +9,26 @@ import os.mkdir
 #for cpd.zips they get moved into a folder: ie 'lsu1234coll1-cpd/lsu1234coll1-cpd.zip' 
 #.zips that are simple only (not containing *cpd* in name) and are > 1Gig in size, must also go in a subfolder. IE: 'loyno-morethan1Gigcoll2-mp3/loyno-morethan1Gigcoll2-mp3.zip'
 
-filelist=['uno-p15140coll1-cpd.zip','uno-p151234coll2-jp2.zip','lsu->1gig-mp3']
+filelist=['uno-p15140coll1-cpd.zip','uno-p151234coll2-jp2.zip','lsu-1gig-mp3']
 
 def cpd_subdir(zip_list):
  for line in zip_list:
  	if 'cpd' in line:
  		print line[:-4]
+ 		#os.mkdir(line[:-4])
+ 		#os.mv(line, line[:-4])
 
-with open('list','r',encoding='utf8') as f:
-    filelist=[i for i in f.readlines()]
+#with open('list','r',encoding='utf8') as f:
+#    filelist=[i for i in f.readlines()]
 
-cpd_subdir(filelist)
+#cpd_subdir(filelist)
 
-#def large_zip_subdir():
+def large_zip_subdir():
+	for line in zip_list:
+		if os.stat(line) > 4000:
+			print('bigfile')
+
+large_zip_subdir(filelist)
 	
 
 #drush commands have 3 forms, cpd-subfolder, simple.zip, >1gig-subfolder/ 
