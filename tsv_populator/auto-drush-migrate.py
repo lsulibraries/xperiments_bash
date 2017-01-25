a8dfab625f990f3dd80e428b7799f7461efa9641
flummingbird <wwconlin@gmail.com>
2017-01-25 10:25:59 -0600

    to do for auto-migrate.py + clearer print statements

--- tsv_populator/auto-drush-migrate.py
+++ tsv_populator/auto-drush-migrate.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-# this script takes a list of input zips (with no repeated namespaces or those with pre-existing collection containers)
-# the script writes a simple .tsv with 'alias => title' based on an input dictionary of alias title associations
-# the script then writes a drush command populated with namespace, and the .tsv file as input.
+# input should be a list of .zip filenames with institution-alias-filetype.zip
+# some aliases included institution, we have altered them for the purpose of this script only
+# script outputs .tsv with 'namespace => title' based on an input dictionary of alias-names file
+# writes a drush command populated with namespace, and the .tsv file as input.
+#------------------------------------------------------------TO DO---------------------------------------------------------------------
+# to add content type flag for drush command, could add all types for default. or could crossref a filetypes => content-model dictionary
+# check for duplicated namespace in list of zips (ie lsu-acc-jp2.zip and lsu-acc-cpd.zip in same list) if true skip second tsv and drush command.
+# check whether ldl.lib.lsu.edu/islandora/object/{namespace}:collection exists yet (curl)
+# incorporate into ingest_aid.py
 import csv
 import re
 
@@ -18,21 +24,22 @@ title = ''
 with open('input', 'r', encoding='utf-8') as f:
     for line in f:
         namespace = line[:-9]
-        print(namespace)
+        print(line + 'namespace = ' + namespace)
         alias = re.search('(-.*)', namespace)
         alias = alias.group(0)
         alias = alias[1:]
-        print(alias)
+        print('ailas = ' + alias)
         title = at_dict[alias]
-        print(title)
+        print('title = ' + title)
         tsvname = namespace + '.tsv'
         tsvcontents = namespace + '\t' + title
         with open(tsvname, 'w') as f:
             f.write(tsvcontents)
+        print('tsv content ' + tsvcontents)
         drushcommand = 'drush --user=admin cicfc --input=/vagrant/%s.tsv  --namespace=%s:collection --parent=islandora:root\n'  %  (namespace, namespace)
         with open('drush-coll-migrate', 'a') as f:
              f.write(drushcommand) 
-        
+        print('drush command = ' + drushcommand)
         #print(tsvcontents, file=tsvname)
         #write drush command to file.
   
