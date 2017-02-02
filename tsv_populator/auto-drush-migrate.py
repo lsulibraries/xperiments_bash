60eacfd01ecce95485b4bda78fe64ccff8019436
flummingbird <wwconlin@gmail.com>
2017-02-02 10:41:47 -0600

    url checks added

--- tsv_populator/auto-drush-migrate.py
+++ tsv_populator/auto-drush-migrate.py
@@ -5,11 +5,11 @@
 # writes a drush command populated with namespace, and the .tsv file as input.
 #------------------------------------------------------------TO DO---------------------------------------------------------------------
 # to add content type flag for drush command, could add all types for default. or could crossref a filetypes => content-model dictionary
-# check for duplicated namespace in input (list of zips) (ie lsu-acc-jp2.zip and lsu-acc-cpd.zip in same list) if true skip second tsv and drush command.
 # check whether ldl.lib.lsu.edu/islandora/object/{namespace}:collection exists yet (curl)
 # incorporate into ingest_aid.py
 import csv
 import re
+import requests
 
 with open('alias-names', 'r', encoding='utf-8') as f:
     csv_reader = csv.reader(f, delimiter='\t')
@@ -17,6 +17,9 @@ with open('alias-names', 'r', encoding='utf-8') as f:
 
 #print(at_dict)
 
+
+prefix_url = 'http://ldl.lib.lsu.edu/islandora/object/'
+check_dup = []
 namespace = ''
 alias = ''
 title = ''
@@ -24,21 +27,32 @@ title = ''
 with open('input', 'r', encoding='utf-8') as f:
     for line in f:
         namespace = line[:-9]
-        print(line + 'namespace = {}'.format(namespace))
-        alias = re.search('(-.*)', namespace)
-        alias = alias.group(0)
-        alias = alias[1:]
-        print('ailas = {}'.format(alias))
-        title = at_dict[alias]
-        print('title = {}'.format(title))
-        tsvname ='{}.tsv'.format(namespace)
-        tsvcontents = namespace + '\t' + title
-        with open(tsvname, 'w') as f:
-            f.write(tsvcontents)
-        print('tsv content = {}'.format(tsvcontents))
-        drushcommand = 'drush --user=admin cicfc --input=/vagrant/{0}.tsv  --namespace={0}:collection --parent=islandora:root\n'.format(namespace)
-        with open('drush-coll-migrate', 'a') as f:
-             f.write(drushcommand) 
-        print('drush command = {}'.format(drushcommand))
+        coll_url = '{0}{1}:collection'.format(prefix_url,namespace)
+        print(coll_url)
+        request = requests.get(coll_url)
+        if request.status_code == 200:
+            print('collection already exists')
+            #how to skip the line if it already exists
+        else:
+            if namespace not in check_dup:
+                check_dup.append(namespace)
+                print(line + 'namespace = {}'.format(namespace))
+                alias = re.search('(-.*)', namespace)
+                alias = alias.group(0)
+                alias = alias[1:]
+                print('ailas = {}'.format(alias))
+                title = at_dict[alias]
+                print('title = {}'.format(title))
+                tsvname ='{}.tsv'.format(namespace)
+                tsvcontents = namespace + '\t' + title
+                with open(tsvname, 'w') as f:
+                    f.write(tsvcontents)
+                print('tsv content = {}'.format(tsvcontents))
+                drushcommand = 'drush --user=admin cicfc --input=/vagrant/{0}.tsv  --namespace={0}:collection --parent=islandora:root\n'.format(namespace)
+                with open('drush-coll-migrate', 'a') as f:
+                    f.write(drushcommand) 
+                print('drush command = {}'.format(drushcommand))
+            else:
+                print('namespace duplicated, skipping tsv creation')
         #print(tsvcontents, file=tsvname)
         #write drush command to file.
