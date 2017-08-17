759d805ff105241883827ecd1c768e7a0986a173
flummingbird <wwconlin@gmail.com>
2017-08-17 13:51:24 -0500

    fixes

--- replace/replace.sh
+++ replace/replace.sh
@@ -3,10 +3,9 @@ counter=1
 p="p"
 counterp=${counter}${p}
 test='tulane record tulane:jrphil193050'
-current=''
 for line in $(cat tulane-orig-ns.txt);do
   bod=$(sed -n ${counterp} < tulane_collections.tsv)
-  echo $bod | sed "s/$test/'tulane record $line'/" >>tmp.tsv
+  echo "$bod" | sed "s/$test/tulane record $line/" >>tmp.tsv
   counter=$((counter+1))
   counterp=${counter}${p}
 done
