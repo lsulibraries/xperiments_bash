644e738ca181767432cd0a57ca6eae81972d03db
William Conlin <wwc@system76-pc.localdomain>
2020-07-17 14:56:14 -0500

    rename files

--- python/organize_.py
+++ python/organize_.py
@@ -27,11 +27,19 @@ def organize_cpd_structure():
             subfolder = directory[1]
             subfolder = subfolder.split('.')
             subdir = objdir + subfolder[0]
+            if '.xml' in line:
+                os.rename(line, 'MODS.xml')
+                line = 'MODS.xml'
+            if '.jp2' in line:
+                os.rename(line, 'OBJ.xml')
+                line = 'OBJ.xml'
             shutil.move(line, subdir)
     filelist = [os.path.abspath(i) for i in os.listdir()]
     for line in filelist:
         if '.xml' in line:
-            shutil.move(line, '{}/'.format(line[:-4]))
+            place = '{}/'.format(line[:-4])
+            os.rename(line, 'MODS.xml')
+            shutil.move('MODS.xml', place)
  
 
 organize_cpd_structure()
